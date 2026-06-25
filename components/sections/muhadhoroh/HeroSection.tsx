59ab442be9f003d25a3d4d128be95d52158e0c68
fakhrialwin5 <fakhrialwin5@gmail.com>
2026-06-25 09:19:11 +0700

    Remove scroll indicator (mouse logo) from all pages

--- components/sections/muhadhoroh/HeroSection.tsx
+++ components/sections/muhadhoroh/HeroSection.tsx
@@ -149,12 +149,6 @@ export default function HeroSection() {
         </motion.div>
       </div>
 
-      {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2">
-        <div className="w-6 h-10 border-2 border-white/40 rounded-full flex justify-center">
-          <div className="w-1 h-3 bg-white/60 rounded-full mt-2 animate-scroll-bounce" />
-        </div>
-      </div>
     </section>
   );
 }
